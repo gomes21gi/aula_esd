bbe1f7a86a92fd437c165eba0acb1279ef6b3a13
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2026-03-20 21:14:35 -0300

    aula de java

---
+++ vetores/src/Vetor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Vetor {
+    int[] vetor;
+    int tamanho;
+    int numElementos;
+
+    public Vetor(int tamanho) {
+        this.tamanho = tamanho;
+        vetor = new int[tamanho];
+        numElementos = 0;
+    }
+}
